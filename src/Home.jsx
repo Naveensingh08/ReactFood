3870f2a4bd35ebb2b52ca5825c756195b16e24f5
Naveensingh08 <naveensinghrajapoot301@gmail.com>
2025-03-03 12:26:06 +0530

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,6 +11,8 @@ function Home() {
 export default Home;
 */
 
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -27,3 +29,68 @@ function Home() {
 }
 
 export default Home;
+
+// import { useSelector } from "react-redux"
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// function Home()
+// {
+//   let vegItem=useSelector((state) => state.product.veg);
+//   let nonVegItem=useSelector((state) => state.product.nonVeg);
+
+//   let vegs = vegItem.map((item, index) => (
+//     <div key={index} className="col-md-3 mb-4">
+//         <div className="card">
+//             <img
+//                 src={item.image}
+//                 alt="Not found"
+//                 className="card-img-top"
+//                 style={{ height: '200px', objectFit: 'cover' }} // Adjust image height
+//             />
+//             <div className="card-body">
+//                 <h5 className="card-title">{item.name}</h5>
+//                 <p className="card-text">₹{item.price}</p>
+//                 <button className="btn btn-primary" onClick={() => dispatch(addToCart(item))}>
+//                     Add to Cart
+//                 </button>
+//             </div>
+//         </div>
+//     </div>
+// ));
+
+
+
+//   let nonVegs = nonVegItem.map((item, index) => (
+//           <div key={index} className="col-md-3 mb-4">
+//               <div className="card">
+//                   <img
+//                       src={item.image}
+//                       alt="Not found"
+//                       className="card-img-top"
+//                       style={{ height: '200px', objectFit: 'cover' }} // Adjust image height
+//                   />
+//                   <div className="card-body">
+//                       <h5 className="card-title">{item.name}</h5>
+//                       <p className="card-text">₹{item.price}</p>
+//                       <button className="btn btn-primary" onClick={() => dispatch(addToCart(item))}>
+//                           Add to Cart
+//                       </button>
+//                   </div>
+//               </div>
+//           </div>
+//       ));
+  
+//   return(
+//     <>
+
+// <div className="row">
+//          {vegs}
+//      </div>
+//     <div className="row">
+//          {nonVegs}
+//      </div>
+
+//     </>
+//   )
+// }
+// export default Home;
+
